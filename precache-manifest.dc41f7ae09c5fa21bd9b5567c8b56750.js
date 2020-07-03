d5e52dbe965535a7b67480041857eaf6b64df9c9
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-07-03 14:26:55 -0700

    Updates

---
+++ precache-manifest.dc41f7ae09c5fa21bd9b5567c8b56750.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40ed2607eac02615d4daee2ddbf5d8d",
+    "url": "/ping-coming-soon-react/index.html"
+  },
+  {
+    "revision": "10c3c7a9920234d35df4",
+    "url": "/ping-coming-soon-react/static/js/2.4c5c8b20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ping-coming-soon-react/static/js/2.4c5c8b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "677b564fd4c04e89c43b",
+    "url": "/ping-coming-soon-react/static/js/main.3806e6a2.chunk.js"
+  },
+  {
+    "revision": "6fb20badbe73e0cfd8d2",
+    "url": "/ping-coming-soon-react/static/js/runtime-main.9f12bc72.js"
+  },
+  {
+    "revision": "92dd5c8fe5e8dde945a6f01abca7e7cc",
+    "url": "/ping-coming-soon-react/static/media/illustration-dashboard.92dd5c8f.png"
+  },
+  {
+    "revision": "e392d534418b8f8c25ecb73731147704",
+    "url": "/ping-coming-soon-react/static/media/logo.e392d534.svg"
+  }
+]);
